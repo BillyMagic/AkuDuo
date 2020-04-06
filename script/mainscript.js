696d66e5717262158debd498a1807327ec04f111
fazi312 <fazi312@gmail.com>
2020-04-06 19:08:17 +0200

    Background URL bar mobile version problem solved

--- script/mainscript.js
+++ script/mainscript.js
@@ -1,1 +1,10 @@
 gsap.from("#header", {duration: 2, opacity: 0, y: -80});
+
+var bg = $("body");
+
+function resizeBackground() {
+    bg.height($(window).height());
+}
+
+$(window).resize(resizeBackground);
+resizeBackground();
