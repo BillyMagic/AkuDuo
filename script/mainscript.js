b0799b033ea1fc8bf52fe682ad7445cc12a1580c
fazi312 <fazi312@gmail.com>
2020-04-08 01:07:27 +0200

    NavBar and logo are animated now

--- script/mainscript.js
+++ script/mainscript.js
@@ -1,10 +1,10 @@
-gsap.from("#header", {duration: 2, opacity: 0, y: -80});
+// gsap.from("#header", {duration: 2, opacity: 0, y: -80});
 
-// var bg = $("body");
+// gsap.from("#header a", {duration: 1, opacity: 0, y: -30});
 
-// function resizeBackground() {
-//     bg.height($(window).height());
-// }
+var tl = new TimelineMax();
+tl.from("#header", {duration: 1, opacity: 0, y: -80});
 
-// $(window).resize(resizeBackground);
-// resizeBackground();
+tl.from("#header a", {duration: 1, opacity: 0, y: -30}, "-2");
+tl.from(".logo div", {duration: 1, opacity: 0, y: 40, ease: Power2.easeOut, stagger: 0.2});
+
