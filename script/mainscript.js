c683a1320bb814034e514ad888f2be1126423d46
fazi312 <fazi312@gmail.com>
2020-04-13 13:46:45 +0200

    Document load fixed, flick problem solved

--- script/mainscript.js
+++ script/mainscript.js
@@ -2,9 +2,20 @@
 
 // gsap.from("#header a", {duration: 1, opacity: 0, y: -30});
 
-var tl = new TimelineMax();
-tl.from("#header", {duration: 1, opacity: 0, y: -80});
+document.addEventListener("DOMContentLoaded", function(event) {
 
-tl.from("#header a", {duration: 1, opacity: 0, y: -30}, "-2");
-tl.from(".logo div", {duration: 1, opacity: 0, y: 40, ease: Power2.easeOut, stagger: 0.2});
+    window.addEventListener("load", function(e) {
+
+        document.body.style.display = "block";
+
+        var tl = new TimelineMax();
+        tl.from("#header", {duration: 1, opacity: 0, y: -80});
+
+        tl.from("#header a", {duration: 1, opacity: 0, y: -30}, "-2");
+        tl.from(".logo div", {duration: 1, opacity: 0, y: 40, ease: Power2.easeOut, stagger: 0.2});
+
+    }, false);
+
+});
+
 
