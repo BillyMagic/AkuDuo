7f6f191209d6606adb8ab4ac0dd8176999b08260
fazi312 <fazi312@gmail.com>
2020-04-16 15:41:53 +0200

    Background animated

--- script/mainscript.js
+++ script/mainscript.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
         document.body.style.display = "block";
 
-        gsap.from(".wrapper", {duration: 1, opacity: 0.3, ease: "power1.out"});
+        gsap.from(".wrapper", {duration: 1, opacity: 0.3});
         gsap.from("#header", {duration: 1, opacity: 0, y: -80});
         gsap.from("#header a", {duration: 1, opacity: 0, y: -30});
         gsap.from(".logo div", {duration: 1, delay: 0.8, opacity: 0, y: 40, ease: Power2.easeOut, stagger: 0.2});
