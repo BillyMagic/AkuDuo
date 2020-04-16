92728a94e1041540e1a1eedb9d43597f0893532c
fazi312 <fazi312@gmail.com>
2020-04-16 15:32:47 +0200

    Background fadeIn animate test

--- script/mainscript.js
+++ script/mainscript.js
@@ -8,11 +8,10 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
         document.body.style.display = "block";
 
-        var tl = new TimelineMax();
-        tl.from("#header", {duration: 1, opacity: 0, y: -80});
-
-        tl.from("#header a", {duration: 1, opacity: 0, y: -30}, "-2");
-        tl.from(".logo div", {duration: 1, opacity: 0, y: 40, ease: Power2.easeOut, stagger: 0.2});
+        gsap.from(".wrapper", {duration: 1, opacity: 0.3, ease: "power1.out"});
+        gsap.from("#header", {duration: 1, opacity: 0, y: -80});
+        gsap.from("#header a", {duration: 1, opacity: 0, y: -30});
+        gsap.from(".logo div", {duration: 1, delay: 0.8, opacity: 0, y: 40, ease: Power2.easeOut, stagger: 0.2});
 
     }, false);
 
